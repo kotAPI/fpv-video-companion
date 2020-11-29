36329bf0ddc8fd3f90247a7a2c1acf12cc598c1a
Pranay Kothapalli <pranaykothapalli@gmail.com>
2020-11-29 12:45:21 +0530

    add: multiple support but broke progress bars :(

--- index.js
+++ index.js
@@ -9,35 +9,25 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 const ffmpeg = require('fluent-ffmpeg')
 const cliProgress = require('cli-progress');
 const _colors = require('colors');
-
+const _ = require("lodash")
 const durationUtils = require("./utils/duration")
 
 ffmpeg.setFfmpegPath(ffmpegPath)
 
-var section = {
-  source_video: 'source.mp4',
-  destination_video: 'testing.mp4',
-  startTime:"00:01:00",
-  endTime:"00:01:04"
-}
-
 let progressInstances = {}
-let numberOfInstances =0;
+let numberOfInstances = 0;
 
-function cutVideo(section){
 
-  const totalVideoDuration = durationUtils.getDuration(section.startTime,section.endTime)
 
-  const multibar = new cliProgress.MultiBar({
-    clearOnComplete: false,
-    hideCursor: true
-}, cliProgress.Presets.shades_grey);
+async function cutVideo(section) {
 
-let ID = numberOfInstances+''
-numberOfInstances++;
+  const totalVideoDuration = durationUtils.getDuration(section.startTime, section.endTime)
 
-progressInstances[ID] = multibar.create(100, 0);
-progressInstances[ID].start(100, 0, {
+  let ID = numberOfInstances + ''
+  numberOfInstances++;
+  console.log(ID)
+  progressInstances[ID] = multibar.create(100, 0);
+  progressInstances[ID].start(100, 0, {
     speed: "N/A"
   });
 
@@ -45,53 +35,83 @@ progressInstances[ID].start(100, 0, {
 
 
   ffmpeg('source/' + section.source_video)
-  .setStartTime(section.startTime)
-  .setDuration(totalVideoDuration)
-  .output('destination/' + section.destination_video)
-  .on('end', function (err) {
-    if (!err) {
-      console.log('')
-      console.log(`CONVERSION DONE! Video saved at  ${section.destination_video}`)
-      console.log('')
-    }else{
-      console.log(err)
-    }
-
-    multibar.stop();
-  })
-  .on('progress', (progress) => {
-
-    /**
-     * Unfortunately, this only outputs 
-     * { 
-     *  frames: 20,
-        currentFps: 0,
-        currentKbps: 1,
-        timemark: '00:00:00.42' 
+    .setStartTime(section.startTime)
+    .setDuration(totalVideoDuration)
+    .output('destination/' + section.destination_video)
+    .on('end', function (err) {
+      if (!err) {
+        console.log('')
+        console.log(`CONVERSION DONE! Video saved at  ${section.destination_video}`)
+        console.log('')
+      } else {
+        console.log(err)
       }
 
-        Need to convert it to % manually
+      
+    })
+    .on('progress', (progress) => {
 
-     */
-    //console.log(progress)
-    var percentage = durationUtils.getElapsedPercentage(totalVideoDuration,progress.timemark)
-    
-    progressInstances[ID].increment();
-    progressInstances[ID].update(percentage,{filename: section.destination_video});
-  })
-  .on('error', function (err) {
-    console.log('error: ', err)
-  }).run()
+      /**
+       * Unfortunately, this only outputs 
+       * { 
+       *  frames: 20,
+          currentFps: 0,
+          currentKbps: 1,
+          timemark: '00:00:00.42' 
+        }
+  
+          Need to convert it to % manually
+  
+       */
+      //console.log(progress)
+      var percentage = durationUtils.getElapsedPercentage(totalVideoDuration, progress.timemark)
+      progressInstances[ID].increment();
+      progressInstances[ID].update(percentage, { filename: section.destination_video });
+    })
+    .on('error', function (err) {
+      console.log('error: ', err)
+    }).run()
 }
 
 
-cutVideo(section)
+var multibar = new cliProgress.MultiBar({
+  clearOnComplete: false,
+  hideCursor: true
+}, cliProgress.Presets.shades_grey);
 
-  /*
-    // NOTES //
-    Learn what mux, demux is
-    =======================
-    References :-
-    =======================
-    FFMPEG - https://ffmpeg.org/about.html
-  */
+
+var start = async()=>{
+
+  
+var sections = [{
+  source_video: 'source.mp4',
+  destination_video: 'testing.mp4',
+  startTime: "00:01:00",
+  endTime: "00:01:04"
+},{
+  source_video: 'source.mp4',
+  destination_video: 'testing2.mp4',
+  startTime: "00:01:05",
+  endTime: "00:01:10"
+}]
+
+
+  _.forEach(sections,async(section)=>{
+    await cutVideo(section)
+  })
+  
+  console.log("ALL DONE")
+  multibar.stop();
+}
+
+start();
+
+
+/*
+  // NOTES //
+  Learn what mux, demux is
+  =======================
+  References :-
+  =======================
+  FFMPEG - https://ffmpeg.org/about.html
+*/
